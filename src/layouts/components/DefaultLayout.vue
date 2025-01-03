9290d6a1da6d7bb44f42b3957251e4d3c92edd15
stonegaze08 <stonegaze0808@gmail.com>
2025-01-03 20:52:55 +0700

    fix: update sidebar logo and sidebar name

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -67,9 +67,10 @@ function selected(route: any, nav: NavLink) {
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
+          <!-- <img class="w-10 h-10" src="../../assets/logo.svg" /> -->
+          <img class="w-10 h-10" src="../../assets/logostonegaze.png" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            StoneGazes
+            StoneGaze
           </h1>
         </RouterLink>
         <div
